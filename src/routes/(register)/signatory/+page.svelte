4ac660cbf861e3eb65df3934f84afe7e3d60da83
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-23 21:22:27 +0800

    feat: add check pattern to signatory id input

--- src/routes/(register)/signatory/+page.svelte
+++ src/routes/(register)/signatory/+page.svelte
@@ -1,6 +1,6 @@
 <form class="space-y-2" method="POST">
 	<label class="block" for="uid"> UID: </label>
-	<input name="uid" required />
+	<input name="uid" pattern="\d{4}-\d{4}-\d{4}-\d{4}" required />
 	<label class="block" for="name"> Name: </label>
 	<input name="name" type="text" required />
 	<input class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" />
--- src/routes/generate/+page.svelte
+++ src/routes/generate/+page.svelte
@@ -13,7 +13,7 @@
 
 <form class="space-y-2" method="POST" use:enhance>
 	<label class="block" for=""> UID: </label>
-	<input name="uid" required />
+	<input name="uid" pattern="\d{4}-\d{4}-\d{4}-\d{4}" required />
 	<label class="block" for="doc_id"> Document Identifier: </label>
 	<input name="doc_id" type="text" required />
 	<input class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" />
