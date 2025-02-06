73b3e38a84ddc437f075e024683dcb5c280dd288
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-06 22:20:49 +0800

    feat: implement auth frontend

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,20 +1,23 @@
-<div class="flex flex-col">
-    <div>
-        <h3>Register</h3>
-        <form action="?/register" method="post">
-            
+<div class="flex flex-row w-auto space-x-5">
+    <div class="space-y-2">
+        <h3 class="text-lg">Register</h3>
+        <form class="flex flex-col space-y-5" action="?/login" method="post">
+            <label class="flex flex-col">
+                Signatory ID
+                <input name="id"/>
+            </label>
+            <label class="flex flex-col">
+                Password
+                <input name="password"/>
+            </label>
+            <button class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</button>
+            <button class="block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" formaction="?/register">Register</button>
         </form>
     </div>
-    <div>
-        <h3>Login</h3>
-        <form action="?/login" method="post">
-            
-        </form>
-    </div>
-    <div>
-        <h3>Logout</h3>
-        <form action="?/logout" method="post">
-        
+    <div class="space-y-2">
+        <h3 class="text-lg">Logout</h3>
+        <form class="flex flex-col space-y-5" action="?/logout" method="post">
+            <button class="block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" formaction="?/register">Register</button>
         </form>
     </div>
 </div>
