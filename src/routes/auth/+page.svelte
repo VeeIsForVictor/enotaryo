4fd8c8b0f5b5e93d8b37676f3ab59c114be9f5fe
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-06 20:51:06 +0800

    feat: define auth route and server files

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,20 @@
+<div class="flex flex-col">
+    <div>
+        <h3>Register</h3>
+        <form action="?/register" method="post">
+            
+        </form>
+    </div>
+    <div>
+        <h3>Login</h3>
+        <form action="?/login" method="post">
+            
+        </form>
+    </div>
+    <div>
+        <h3>Logout</h3>
+        <form action="?/logout" method="post">
+        
+        </form>
+    </div>
+</div>
