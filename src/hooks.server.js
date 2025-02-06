e66f20ef8483783a8d534ba2d098c1294ac05294
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-07 00:17:36 +0800

    fix: remove duplicate hooks.server file

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,14 +1,32 @@
 import { building } from '$app/environment';
+import * as auth from '$lib/server/auth';
 
-export async function handle({ event, resolve }) {
+export async function handle ({ event, resolve }) {
 	if (building) return await resolve(event);
-
+	
 	const [{ db }] = await Promise.all([import('$lib/server/db')]);
-
+	
 	event.locals.ctx = {
 		db
 	};
-
-	const response = await resolve(event);
-	return response;
-}
+	
+	const sessionToken = event.cookies.get(auth.sessionCookieName);
+	
+	if (!sessionToken) {
+		event.locals.user = null;
+		event.locals.session = null;
+		return resolve(event);
+	}
+	
+	const { session, user } = await auth.validateSessionToken(sessionToken);
+	if (session) {
+		auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
+	} else {
+		auth.deleteSessionTokenCookie(event);
+	}
+	
+	event.locals.user = user;
+	event.locals.session = session;
+	
+	return resolve(event);
+};
