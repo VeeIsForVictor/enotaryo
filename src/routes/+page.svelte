4c46e5b389988d575190f2a72d2a8652f73f14cd
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-08 22:39:43 +0800

    feat: add link to page to check pending transactions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
+<script lang="ts">
+	import type { LayoutData } from "./$types";
+
+	const { data }: { data: LayoutData } = $props();
+	const { user } = data;
+</script>
+
 <div class="prose">
 	<h3 class="h3">Hello, world! Welcome to the ENotaryo Frontend!</h3>
 	<!-- <a href="/document">Register Documents</a><br /> -->
 	<a href="/signatory">Register Signatories</a><br />
 	<a href="/generate">Generate a QR</a><br />
 	<a href="/read">Read a QR</a><br />
+	{#if user}
+		<a href="/otp">Check pending transactions</a>
+	{/if}
 </div>
