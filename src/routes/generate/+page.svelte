1a01b5c9e256057da062d4f600895baa2714a9e6
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-09 15:25:40 +0800

    fix: ensure signatory input does not reset on submit

--- src/routes/generate/+page.svelte
+++ src/routes/generate/+page.svelte
@@ -5,22 +5,26 @@
 	let outputCanvas: HTMLCanvasElement;
 
 	let { form, data } = $props();
-	const { user } = data;
+	let { user } = $state(data);
 
 	$effect(() => {
 		if (form) QRCode.toCanvas(outputCanvas, form.qrData);
 	});
 </script>
 
-<form class="space-y-2" method="POST" use:enhance>
+<form class="space-y-2" method="POST" use:enhance={() => {
+	return async ({ update }) => {
+		await update({ reset: false });
+	}
+}}>
 	<label class="block" for=""> UID: </label>
 	<input
 		name="uid"
 		pattern={'[a-Z0-9]{4}-[a-Z0-9]{4}-[a-Z0-9]{4}-[a-Z0-9]{4}'}
-		required
-		disabled
 		class="valid:text-green-500 invalid:text-gray-500"
-		value={user}
+		bind:value={user}
+		required
+		readonly
 	/>
 	<input
 		class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
