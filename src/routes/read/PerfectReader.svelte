e027d78d952bce21aa8b3f3913b5710d7dbf5416
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-09 16:54:17 +0800

    feat: implement perfect reader

---
+++ src/routes/read/PerfectReader.svelte
@@ -0,0 +1,10 @@
+<form class="space-y-2 my-4" action="post">
+    <label class="flex flex-col">
+        Session UUID
+        <input class="max-w-md" type="text" name="session" />
+    </label>
+    <input 
+        type="submit" 
+        class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    />
+</form>
