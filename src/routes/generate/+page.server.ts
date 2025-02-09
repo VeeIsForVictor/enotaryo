ad4209b509c1fb454425776a1a4d0cb0a75b1123
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-09 15:13:04 +0800

    fix: fix uid for generation route

--- src/routes/generate/+page.server.ts
+++ src/routes/generate/+page.server.ts
@@ -1,3 +1,11 @@
+import { redirect } from '@sveltejs/kit';
+
+export async function load({ parent }) {
+	const { user } = await parent();
+	if (!user) redirect(302, "/");
+	return { user };
+}
+
 export const actions = {
 	default: async ({ request, fetch }) => {
 		const formData = await request.formData();
--- src/routes/generate/+page.svelte
+++ src/routes/generate/+page.svelte
@@ -4,7 +4,8 @@
 
 	let outputCanvas: HTMLCanvasElement;
 
-	let { form } = $props();
+	let { form, data } = $props();
+	const { user } = data;
 
 	$effect(() => {
 		if (form) QRCode.toCanvas(outputCanvas, form.qrData);
@@ -17,7 +18,9 @@
 		name="uid"
 		pattern={'[a-Z0-9]{4}-[a-Z0-9]{4}-[a-Z0-9]{4}-[a-Z0-9]{4}'}
 		required
+		disabled
 		class="valid:text-green-500 invalid:text-gray-500"
+		value={user}
 	/>
 	<input
 		class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
