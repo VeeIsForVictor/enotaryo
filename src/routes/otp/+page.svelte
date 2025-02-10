4b182592b5dd87a527d1f0e997b3d851a8b2db48
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-10 13:12:23 +0800

    feat: add rudimentary frontend for otp view

---
+++ src/routes/otp/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    const { data } = $props();
+
+    const { results } = data;
+</script>
+
+{#each results as {id, isVerified}}
+    <p>Transaction with id {id}, verified: {isVerified}</p>
+{/each}
