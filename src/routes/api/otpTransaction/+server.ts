e96020276c1dc268de26b118894f46f7b778048c
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-10 11:00:32 +0800

    feat: implement status checks in otp transactions

--- src/routes/api/otpTransaction/+server.ts
+++ src/routes/api/otpTransaction/+server.ts
@@ -1,4 +1,6 @@
-import type { RequestHandler } from '@sveltejs/kit';
+import { getSessionStatus } from '$lib/server/db';
+import { error, type RequestHandler } from '@sveltejs/kit';
+import { strict } from 'assert';
 
 // Get all OTP transactions for a user
 export const GET: RequestHandler = async ({ locals: { ctx }, request }) => {
@@ -6,17 +8,41 @@ export const GET: RequestHandler = async ({ locals: { ctx }, request }) => {
 };
 
 // Create a new OTP transaction
-export const POST: RequestHandler = async ({ locals: { ctx: { db, logger } }, request }) => {
+export const POST: RequestHandler = async ({ locals: { ctx }, request }) => {
 	
-	// check session status
-	// 	does it actually exist?
-	// 	is it already being verified?
-	// 	is it already verified?
+	strict(typeof ctx != 'undefined');
+	const { db, logger } = ctx;
+
+	const { sessionId } = await request.json();
+	strict(sessionId != null && typeof sessionId == 'string');
+
+	try {
+		// check session status
+		const [sessionStatus, ...rest] = await getSessionStatus(db, sessionId);
+		strict(rest.length == 0);
+
+		// 	does it actually exist?
+		strict(sessionStatus);
+		strict(sessionStatus.id);
+		
+		// 	is it already verified?
+		if (sessionStatus.isVerified || sessionStatus.txnId) {
+			return new Response(JSON.stringify({
+				txnId: sessionStatus.txnId,
+				isVerified: sessionStatus.isVerified
+			}));
+		}
+	}
+	catch (e) {
+		logger.error({ e });
+		return error(500, 'an internal error occurred');
+	}
 
 	// issue the transaction
 	// 	save the transaction id to the database
+	// return the txn id
 	
-	return new Response();
+	return new Response(JSON.stringify({ transactionId }));
 };
 
 // Update the OTP transaction by verifying a candidate OTP code
