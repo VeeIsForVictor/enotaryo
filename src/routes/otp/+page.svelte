cef433a543cde9a7b429583060f58b9990d6a21a
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-10 13:27:10 +0800

    feat: add use:enhance to form

--- src/routes/otp/+page.svelte
+++ src/routes/otp/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
+
     const { data } = $props();
 
     const { results } = data;
@@ -7,10 +9,12 @@
 {#each results as {id, isVerified}}
     {@const txnId=id}
     <p>Transaction with id {id}, verified: {isVerified}</p>
-    <form class="flex flex-col space-y-2 my-4 max-w-md" method="post">
-        <input type="text" name="id" id="id" readonly hidden value={txnId}>
-        <label for="otp">One-time Password</label>
-        <input type="text" name="otp" id="otp">
-        <input class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
-    </form>
+    {#if !isVerified}
+        <form class="flex flex-col space-y-2 my-4 max-w-md" method="post" use:enhance>
+            <input type="text" name="id" id="id" readonly hidden value={txnId}>
+            <label for="otp">One-time Password</label>
+            <input type="text" name="otp" id="otp">
+            <input class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
+        </form>
+    {/if}
 {/each}
