1130b2a2ae8444d87a3b5752a3dc0d0cc7c9701d
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-10 10:22:35 +0800

    feat: add read form handler

--- src/routes/read/+page.server.ts
+++ src/routes/read/+page.server.ts
@@ -1,3 +1,27 @@
+import { strict } from 'assert';
 export const actions = {
-	default: () => {}
+	default: async ({ request, fetch, locals: { ctx } }) => {
+
+		const formData = await request.formData();
+		const sessionId = formData.get('sessionId');
+		
+		strict(typeof ctx !== 'undefined');
+		strict(sessionId != null);
+
+		const readData = { sessionId };
+		const body = JSON.stringify(readData);
+		const response = await fetch('/otpTransaction', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body
+		});
+
+		if (!response.ok) {
+			return response.body;
+		}
+
+		return {};
+	}
 };
