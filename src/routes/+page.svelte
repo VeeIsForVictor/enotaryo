14daf1e524b8b4ac390dd3cdc047edc03e39ed15
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-16 01:33:52 +0800

    fix: remove unused qr generation route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 	<h3 class="h3">Hello, world! Welcome to the ENotaryo Frontend!</h3>
 	<!-- <a href="/document">Register Documents</a><br /> -->
 	<a href="/signatory">Register Signatories</a><br />
-	<a href="/generate">Generate a QR</a><br />
+	<!-- <a href="/generate">Generate a QR</a><br /> -->
 	<a href="/read">Read a QR</a><br />
 	{#if user}
 		<a href="/otp">Check pending transactions</a>
