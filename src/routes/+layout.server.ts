cafb08da569ee5f281eaa937f8360b053a89f09c
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-18 15:36:31 +0800

    feat: add push subscription getter in layout.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,11 @@
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
+	const pushSubscriptionRequest = await fetch('/api/push');
+	const pushSubscription: PushSubscriptionJSON | null = await pushSubscriptionRequest.json();
+
 	if (event.locals.user) {
-		return { user: event.locals.user.signatoryId, pushSubscription: null };
+		return { user: event.locals.user.signatoryId, pushSubscription };
 	} else {
 		return { user: null, pushSubscription: null };
 	}
