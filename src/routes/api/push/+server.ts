378f469482894c19e1d523c012c195ac446808c1
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-22 02:00:35 +0800

    feat: add better logging and error catching in /api/push

--- src/routes/api/push/+server.ts
+++ src/routes/api/push/+server.ts
@@ -6,16 +6,34 @@ export const GET: RequestHandler = async ({ locals: { ctx, user } }) => {
     strict(typeof ctx !== 'undefined');
     if (!user) return error(401, 'unidentified user');
     
-    const subscription = await getPushSubscription(ctx.db, user.id);
+    const [subscription, ...rest] = await getPushSubscription(ctx.db, user.id);
+    if (rest.length != 0) ctx.logger.warn({ rest }, 'length of rest not zero')
+    
+    try {
+        const { pushSubscription } = subscription;
+        
+        ctx.logger.info({ user, pushSubscription }, 'subscription for user retrieved');
 
-    return new Response(JSON.stringify(subscription));
+        return new Response(JSON.stringify({ pushSubscription }));
+    } 
+    catch (e) {
+        ctx.logger.warn({ e }, 'an error occurred while trying to coerce pushSubscription out of subscription')
+
+        return new Response(JSON.stringify({ pushSubscription: null }));
+    }
 }
 
 export const POST: RequestHandler = async ({ locals: { ctx, user }, request }) => {
     strict(typeof ctx !== 'undefined');
     if (!user) return error(401, 'unidentified user');
 
-    const subscription = await upsertPushSubscription(ctx.db, user.id, await request.json());
+    const pushSubscription = await request.json();
+
+    ctx.logger.info({ pushSubscription }, 'received subscription for user');
+
+    const subscription = await upsertPushSubscription(ctx.db, user.id, pushSubscription);
+
+    ctx.logger.info({ user }, 'subscription for user generated');
 
     return new Response(JSON.stringify(subscription));
 }
