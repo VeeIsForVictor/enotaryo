a76f96297bc3e236edf6c4cf6196e7bc39aeac0a
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-23 02:09:53 +0800

    feat: implement async sendOtpNotification helper

---
+++ src/lib/server/notifications/index.ts
@@ -0,0 +1,28 @@
+import { sendNotification } from "$lib/webpush";
+import type { Logger } from "pino";
+import { getPushSubscriptionBySignatoryId, type Interface } from "../db";
+import { strict, strictEqual } from 'assert';
+
+export async function sendOtpNotification(db: Interface, logger: Logger, transactionId: string, signatoryId: string): Promise<string> {
+    const [{ pushSubscription }, ...rest] = await getPushSubscriptionBySignatoryId(db, signatoryId);
+    strict(pushSubscription);
+    strictEqual(rest.length, 0);
+
+    // try to send the push notification
+    try {
+        const sendResult = sendNotification(
+            pushSubscription, 
+            "New Document for Verification", 
+            `A transaction with ID ${transactionId} has been filed for your verification.`
+        );
+
+        logger.info({ sendResult }, 'push notification dispatched');
+
+        return transactionId;
+    }
+    catch (e) {
+        logger.error({ e }, 'an error occurred while dispatching a push notification');
+
+        throw new Error(`an error occurred while sending a notification for ${transactionId}`);
+    }
+}
