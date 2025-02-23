6daf3f470ed482f2225df6cd0120383035b59b02
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-23 17:04:15 +0800

    feat: add server-side validation for signature POST-ing

--- src/routes/api/signature/+server.ts
+++ src/routes/api/signature/+server.ts
@@ -1,11 +1,21 @@
 import { error, type RequestHandler } from '@sveltejs/kit';
 import { strict } from 'assert';
 import { insertSignature } from '$lib/server/db';
+import { NewSignature } from '$lib/models/signature';
+import { safeParse } from 'valibot';
 
 export const POST: RequestHandler = async ({ locals: { ctx }, request }) => {
-	const { sigId } = await request.json();
+	const requestJson = await request.json();
+	const newSignatureResult = safeParse(NewSignature, requestJson);
 
 	strict(typeof ctx != 'undefined');
+
+	if (!newSignatureResult.success) {
+		ctx.logger.error({ requestJson }, 'malformed new signature request');
+		return error(400, { message: 'malformed new signature request' });
+	}
+
+	const { signatoryId: sigId } = newSignatureResult.output as NewSignature;
 
 	ctx.logger.info({ sigId });
 
