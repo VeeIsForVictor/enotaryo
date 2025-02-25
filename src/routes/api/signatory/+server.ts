90a0659cffb8fbc8acd99412251018e5ba5ac5cd
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-02-25 14:24:16 +0800

    feat: add better error handling for api/signatory

--- src/routes/api/signatory/+server.ts
+++ src/routes/api/signatory/+server.ts
@@ -17,13 +17,22 @@ export const POST: RequestHandler = async ({ locals: { ctx }, request }) => {
 
 	const { id } = newSignatoryResult.output as NewSignatory;
 
-	ctx.logger.info({ id });
+	ctx.logger.info({ id }, 'attempting to insert signatory with id');
 
 	const start = performance.now();
-	insertSignatory(ctx.db, id);
+	
+	try {
+		await insertSignatory(ctx.db, id);
+	}
+	catch (e) {
+		ctx.logger.error({ e }, 'error occurred while trying to insert signatory')
+
+		return error(500, { message: 'signatory insertion failed due to server-side error' })
+	}
+	
 	const signatoryInsertTime = performance.now() - start;
 
-	ctx.logger.info({ signatoryInsertTime });
+	ctx.logger.info({ signatoryInsertTime }, 'new signatory inserted');
 
 	return new Response();
 };
