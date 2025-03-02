3186aed5003ec8b2e6286b1a83a698fc72cbb1f2
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-03-02 18:18:47 +0800

    feat: add logging to document reading

--- src/routes/(register)/document/+page.server.ts
+++ src/routes/(register)/document/+page.server.ts
@@ -1,7 +1,11 @@
 import type { Actions } from '@sveltejs/kit';
+import { env } from '$env/dynamic/public';
+import { strict } from 'assert';
 
 export const actions: Actions = {
-	default: async ({ request, fetch }) => {
+	default: async ({ request, fetch, locals: { ctx } }) => {
+		strict(typeof ctx !== 'undefined');
+
 		interface Data {
 			title: string;
 			file: string;
@@ -19,13 +23,31 @@ export const actions: Actions = {
 
 		const data: Data = { title: formData.get('title') as string, file: blobData as string }
 
-		await fetch('/api/document', {
+		const documentId = await fetch('/api/document', {
 			method: 'post',
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(data)
 		});
+
+		ctx.logger.info({ documentId }, 'new document POST-ed')
+
+		// retrieve signatures
+		const response = await fetch(`${env.PUBLIC_QR_API}/document/`, {
+			method: 'post',
+			body: formData
+		});
+
+		let signatures: string[] | object[] = await response.json()
+
+		signatures = signatures.map((str) => JSON.stringify(str))
+
+		ctx.logger.info({ signatures }, 'signatures retrieved from document');
+
+		// store signatures
+
+		// issue signature verification
 
 		return { success: true };
 	}
