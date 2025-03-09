6c6a4aad4eb60781db5ea62c0585e0f6bb8d30e4
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-03-09 15:03:24 +0800

    feat: add labels

--- src/routes/public/+page.svelte
+++ src/routes/public/+page.svelte
@@ -8,7 +8,7 @@
 	{#each documents as { id, title, signatureCount, signatoryCount }}
 		<div class="flex-col border-slate-800 border-solid border-2 p-1">
 			<p>Document with id {id}, title: {title}</p>
-            <p>{signatureCount} / {signatoryCount}</p>
+            <p>{signatureCount} signatures verified / {signatoryCount} signatures required</p>
 		</div>
 	{/each}
 </div>
