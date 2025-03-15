76a1e7b686cc8f3e744ece6ccb727cee3669f5d9
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-03-16 04:03:30 +0800

    feat: add linking to individual public documents

--- src/routes/public/+page.svelte
+++ src/routes/public/+page.svelte
@@ -5,14 +5,19 @@
 </script>
 
 <div class="space-y-1">
-	{#each documents as { id, title, signatureCount, signatoryCount, signatories }}
-		<div class="flex-col border-slate-800 border-solid border-2 p-1">
-			<p>Document with id {id}, title: {title}</p>
-			<p>{signatureCount} signatures verified / {signatoryCount} signatures required</p>
-			{#each signatories as { signatoryId, isVerified }}
-				<p>{isVerified ? '✅' : '❎'} {signatoryId}</p>
-			{/each}
-			<p>TODO: Add image of the document here</p>
-		</div>
+	{#each documents as { id, title, signatureCount, signatoryCount }}
+	{#if signatureCount == signatoryCount}
+		<a href="/public/{id}">
+			<div class="flex-col border-slate-800 border-solid border-2 p-1 hover:bg-gray-400">
+				<p>Document with id {id}, title: {title}</p>
+			</div>
+		</a>
+	{:else}
+		<a href="/public/{id}">
+			<div class="flex-col border-green-800 text-green-900 bg-green-300 border-solid border-2 p-1 hover:bg-green-400">
+				<p>Document with id {id}, title: {title}</p>
+			</div>
+		</a>
+	{/if}
 	{/each}
 </div>
