19c562e150d60823730b8f98e85074d558f10ea7
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-03-16 18:30:30 +0800

    fix: ensure document highlighting only occurs for complete signatory counts

--- src/routes/public/+page.svelte
+++ src/routes/public/+page.svelte
@@ -6,18 +6,16 @@
 
 <div class="space-y-1">
 	{#each documents as { id, title, signatureCount, signatoryCount }}
-	{#if signatureCount == signatoryCount}
-		<a href="/public/{id}">
-			<div class="flex-col border-slate-800 border-solid border-2 p-1 hover:bg-gray-400">
-				<p>Document with id {id}, title: {title}</p>
-			</div>
-		</a>
-	{:else}
-		<a href="/public/{id}">
-			<div class="flex-col border-green-800 text-green-900 bg-green-300 border-solid border-2 p-1 hover:bg-green-400">
-				<p>Document with id {id}, title: {title}</p>
-			</div>
-		</a>
-	{/if}
+	<a href="/public/{id}">
+		{#if signatureCount != signatoryCount}
+				<div class="flex-col border-slate-800 border-solid border-2 p-1 hover:bg-gray-400">
+					<p>Document with id {id}, title: {title}</p>
+				</div>
+		{:else}
+				<div class="flex-col border-green-800 text-green-900 bg-green-300 border-solid border-2 p-1 hover:bg-green-400">
+					<p>Document with id {id}, title: {title}</p>
+				</div>
+		{/if}
+	</a>
 	{/each}
 </div>
