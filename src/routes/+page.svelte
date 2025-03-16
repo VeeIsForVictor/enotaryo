1c00ba67cc3e320ba4f4367d911bb9de23a1b57a
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-03-16 13:34:30 +0800

    feat: remove unused links in home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,7 @@
 
 <div class="prose">
 	<h3 class="h3">Hello, world! Welcome to the ENotaryo Frontend!</h3>
-	<!-- <a href="/document">Register Documents</a><br /> -->
 	<a href="/signatory">Register Signatories</a><br />
-	<!-- <a href="/generate">Generate a QR</a><br /> -->
-	<a href="/read">Read a QR</a><br />
 	<a href="/document">Upload a Document</a><br />
 	{#if user}
 		<a href="/otp">Check pending transactions</a><br />
