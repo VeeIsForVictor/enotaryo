f253fbb45ef43b1414f59fbefc8ad7bd0c45953b
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-03-16 13:52:45 +0800

    extract information from document in public/[id] route

--- src/routes/public/[id]/+page.svelte
+++ src/routes/public/[id]/+page.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
+	import { error } from '@sveltejs/kit';
+
 	const { data } = $props();
 
 	const { documents } = data;
+
+	const [ document, ...rest ] = documents;
+	if (rest.length != 0) error(421);
+
+	const { id, title, file, signatureCount, signatoryCount, signatories } = document;
+
 </script>
 
 <div class="space-y-1">
-	{#each documents as { id, title, file, signatureCount, signatoryCount, signatories }}
-		<a href="/public/{id}">
-			<div class="flex-col border-slate-800 border-solid border-2 p-1">
-				<p>Document with id {id}, title: {title}</p>
-				<p>{signatureCount} signatures verified / {signatoryCount} signatures required</p>
-				{#each signatories as { signatoryId, isVerified }}
-					<p>{isVerified ? '✅' : '❎'} {signatoryId}</p>
-				{/each}
-                {@debug file}
-				<p>TODO: Add image of the document here</p>
-			</div>
-		</a>
-	{/each}
+	<a href="/public/{id}">
+		<div class="flex-col border-slate-800 border-solid border-2 p-1">
+			<p>Document with id {id}, title: {title}</p>
+			<p>{signatureCount} signatures verified / {signatoryCount} signatures required</p>
+			{#each signatories as { signatoryId, isVerified }}
+				<p>{isVerified ? '✅' : '❎'} {signatoryId}</p>
+			{/each}
+			{@debug file}
+			<p>TODO: Add image of the document here</p>
+		</div>
+	</a>
 </div>
