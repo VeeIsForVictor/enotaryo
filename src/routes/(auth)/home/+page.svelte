df95421d4be7df06530820dd471bfefa60552885
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-03-19 03:33:18 +0800

    feat: add button for document uploads to homepage

--- src/routes/(auth)/home/+page.svelte
+++ src/routes/(auth)/home/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import UserCard from "$lib/components/UserCard.svelte";
+	import { Button } from "flowbite-svelte";
 	import type { LayoutData } from "../$types";
+	import { CirclePlusOutline } from "flowbite-svelte-icons";
 
     const { data }: { data: LayoutData } = $props();
 
@@ -8,6 +10,8 @@
 </script>
 
 
-<UserCard {user}>
-
-</UserCard>
+<div class="flex flex-col space-y-5">
+    <UserCard {user} />
+    
+    <Button class="bg-purple-800 hover:bg-purple-900" href="/document"><CirclePlusOutline class="mr-2"/>Add new documents for notarization</Button>
+</div>
