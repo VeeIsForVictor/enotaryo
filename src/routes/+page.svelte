532305b4558c3ae6b3ba88e98729c06a0b45c4ca
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-03-18 20:55:38 +0800

    feat: rework layout for homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,15 @@
 	const { user } = data;
 </script>
 
-<div class="prose">
-	<h3 class="h3">Hello, world! Welcome to the ENotaryo Frontend!</h3>
-	<a href="/signatory">Register Signatories</a><br />
-	<a href="/document">Upload a Document</a><br />
-	{#if user}
-		<a href="/otp">Check pending transactions</a><br />
-	{/if}
-	<a href="/public">View Documents</a><br />
+<div class="flex flex-col items-center justify-center gap-y-5 my-auto">
+	<img src="/enotaryo-logo.png" alt="logo of the app" class="bg-purple-300 rounded-3xl" height=256 width=256/>
+	<div class="prose flex flex-col items-center">
+		<h1>Welcome to ENotaryo</h1>
+		<a href="/signatory">Register Signatories</a>
+		<a href="/document">Upload a Document</a>
+		{#if user}
+			<a href="/otp">Check pending transactions</a>
+		{/if}
+		<a href="/public">View Documents</a>
+	</div>
 </div>
