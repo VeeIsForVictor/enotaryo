8fbfd9d97bb43fa8768f09cea678a6d4aae95f08
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-03-19 03:49:26 +0800

    feat: add tabs for document lists

--- src/routes/(auth)/home/+page.svelte
+++ src/routes/(auth)/home/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import UserCard from "$lib/components/UserCard.svelte";
-	import { Button } from "flowbite-svelte";
+	import { Button, Tabs, TabItem } from "flowbite-svelte";
 	import type { LayoutData } from "../$types";
-	import { CirclePlusOutline } from "flowbite-svelte-icons";
+	import { CheckCircleOutline, CirclePlusOutline, ClockOutline } from "flowbite-svelte-icons";
 
     const { data }: { data: LayoutData } = $props();
 
@@ -10,8 +10,17 @@
 </script>
 
 
-<div class="flex flex-col space-y-5">
+<div class="flex flex-col space-y-5 w-max mx-auto">
     <UserCard {user} />
     
-    <Button class="bg-purple-800 hover:bg-purple-900" href="/document"><CirclePlusOutline class="mr-2"/>Add new documents for notarization</Button>
+    <Button class="bg-purple-800 hover:bg-purple-900" href="/document"><CirclePlusOutline class="mr-2 inline"/>Add new documents for notarization</Button>
+
+    <Tabs tabStyle="full" defaultClass="flex rounded-lg divide-x rtl:divide-x-reverse divide-gray-200 shadow-sm dark:divide-gray-700 w-auto">
+        <TabItem open>
+            <div class="w-max px-10" slot="title"><ClockOutline class="mr-2 inline"/> Pending Documents</div>
+        </TabItem>
+        <TabItem>
+            <div class="w-max px-10" slot="title"><CheckCircleOutline class="mr-2 inline"/> Completed Documents</div>
+        </TabItem>
+    </Tabs>
 </div>
