abf613f77f673c417ff99aeae6199bd7d9e96a96
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-03-18 21:19:17 +0800

    feat: add public archive button to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,8 @@
 	const { data, form }: { data: LayoutData, form: ActionData } = $props();
 	const { user } = data;
 
+	import { Button } from 'flowbite-svelte';
+
 	const validationString = validateIdNumber.source;
 </script>
 
@@ -14,7 +16,7 @@
 	<div class="prose flex flex-col items-center">
 		<h1>Welcome to ENotaryo</h1>
 		<div class="flex flex-row w-auto space-x-5">
-			<div class="space-y-2">
+			<div class="flex flex-col space-y-5">
 				{#if !user}
 					<form class="flex flex-col space-y-5" action="?/login" method="post">
 						<label class="flex flex-col">
@@ -29,29 +31,32 @@
 						<label class="flex flex-col">
 							Password
 							<input type="password" name="password" />
+							<div>
+								<p style="color: red">{form?.message ?? ''}</p>
+							</div>
 						</label>
 						<button
 							class="block bg-purple-700 hover:bg-purple-800 text-white font-bold py-2 px-4 rounded-sm"
-							>Login</button
-						>
+							>Login</button>
 						<button
 							class="block bg-purple-400 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded-sm"
-							formaction="?/register">Register</button
-						>
-					</form>
-				{:else}
-				<h3 class="text-lg">You are logged in as {user}</h3>
-					<form class="flex flex-col space-y-5" action="?/logout" method="post">
-						<button
+							formaction="?/register">Register</button>
+						</form>
+						{:else}
+						<h3 class="text-lg">You are logged in as {user}</h3>
+						<form class="flex flex-col space-y-5" action="?/logout" method="post">
+							<button
 							class="block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-sm"
 							formaction="?/logout">Logout</button
-						>
-					</form>
-				{/if}
+							>
+						</form>
+						{/if}
+				<a href="/public" class="no-underline">
+					<button class="block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-sm w-full">
+						Public Archive
+					</button>
+				</a>
 			</div>
-		</div>
-		<div>
-			<p style="color: red">{form?.message ?? ''}</p>
 		</div>
 	</div>
 </div>
