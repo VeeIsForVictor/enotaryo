4c6243d04414b66c24106a7b5b5ac5a9d06e3e61
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-03-24 12:33:36 +0800

    feat: add denial indicator to public documents

--- src/routes/public/+page.svelte
+++ src/routes/public/+page.svelte
@@ -5,16 +5,22 @@
 </script>
 
 <div class="space-y-1">
-	{#each documents as { id, title, signatureCount, signatoryCount }}
+	{#each documents as { id, title, signatureCount, signatoryCount, denyingCount }}
 		<a href="/public/{id}">
-			{#if signatureCount != signatoryCount}
-				<div class="flex-col border-slate-800 border-solid border-2 p-1 hover:bg-gray-400">
-					<p>Document with id {id}, title: {title}</p>
-				</div>
-			{:else}
+			{#if signatureCount == signatoryCount}
 				<div
 					class="flex-col border-green-800 text-green-900 bg-green-300 border-solid border-2 p-1 hover:bg-green-400"
 				>
+					<p>Document with id {id}, title: {title}</p>
+				</div>
+			{:else if denyingCount > 0}
+				<div
+					class="flex-col border-red-800 text-red-900 bg-red-300 border-solid border-2 p-1 hover:bg-red-400"
+				>
+					<p>Document with id {id}, title: {title}</p>
+				</div>
+			{:else}
+				<div class="flex-col border-slate-800 border-solid border-2 p-1 hover:bg-gray-400">
 					<p>Document with id {id}, title: {title}</p>
 				</div>
 			{/if}
