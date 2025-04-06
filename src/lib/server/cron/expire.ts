a3058912e9c4d78e34f97cb0ff691dd92851af68
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-04-07 01:50:48 +0800

    feat: record time for routine d1

--- src/lib/server/cron/expire.ts
+++ src/lib/server/cron/expire.ts
@@ -11,7 +11,14 @@ import type { Logger } from 'pino';
 export function setupCronExpire(db: Interface, logger: Logger) {
 	cron.schedule('0 0 * * *', async () => {
 		logger.warn('cron job to check for expired documents running');
-		for (const document of await getDocuments(db)) {
+
+		const docGetStart = performance.now();
+		const documents =  await getDocuments(db);
+		const docGetTime = performance.now() - docGetStart;
+
+		logger.info({ routine: "d1", time: docGetTime }, 'routine d1');
+
+		for (const document of documents) {
 			const { id, title } = document;
 			logger.info({ id, title }, 'checking document if expired');
 			if (document.uploadTime < new Date(Date.now() - 1000 * 60 * 60 * 24 * 2)) {
