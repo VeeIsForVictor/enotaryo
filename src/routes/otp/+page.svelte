b7a58926afcb22d4c3032c2a358f624cf2a3d916
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-05-18 19:59:11 +0800

    fix: correct typos in data testid for buttons

--- src/routes/otp/+page.svelte
+++ src/routes/otp/+page.svelte
@@ -31,12 +31,12 @@
 					<input
 						class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-sm"
 						type="submit"
-						data-test-id="approve-{documentTitle}"
+						data-testid="approve-{documentTitle}"
 					/>
 					<button
 						class="block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-sm"
 						formaction="?/deny"
-						data-test-id="deny-{documentTitle}"
+						data-testid="deny-{documentTitle}"
 					>
 						Deny
 					</button>
